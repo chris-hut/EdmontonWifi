81e6237fabf0efa3fe47426181b8ec665f9b2d43
Chris Hut <tophernuts@gmail.com>
2014-04-15 21:01:09 -0700

    Added a bunch of comparators plus the Wifi.distance variable

--- src/hey/rich/edmontonwifi/Wifi.java
+++ src/hey/rich/edmontonwifi/Wifi.java
@@ -1,5 +1,7 @@
 package hey.rich.edmontonwifi;
 
+import java.util.Comparator;
+
 import android.location.Location;
 
 /**
@@ -7,6 +9,8 @@ import android.location.Location;
  * assets/wifi.json
  */
 public class Wifi {
+
+	private static float INVALID_DISTANCE = -1;
 
 	public enum Facility {
 		CITY, TRANSIT;
@@ -33,6 +37,8 @@ public class Wifi {
 	private String provider;
 	/** Spatial coordinates of location */
 	private Location location;
+	/** Distance to a specified location */
+	private double distance;
 
 	public Wifi(String id, String name, String address, Facility facility,
 			Status status, String provider, Location location) {
@@ -43,6 +49,10 @@ public class Wifi {
 		this.status = status;
 		this.provider = provider;
 		this.location = location;
+
+		// Set distance to invalid since we don't know what location we want
+		// distance from
+		this.distance = -INVALID_DISTANCE;
 	}
 
 	public String getId() {
@@ -80,8 +90,8 @@ public class Wifi {
 	public Status getStatus() {
 		return status;
 	}
-	
-	public String getStatusString(){
+
+	public String getStatusString() {
 		return status.toString();
 	}
 
@@ -103,6 +113,79 @@ public class Wifi {
 
 	public void setLocation(Location location) {
 		this.location = location;
+	}
+
+	public void setDistance(Location l) {
+		this.distance = l.distanceTo(this.location);
+	}
+
+	public double getDistance() {
+		return this.distance;
+	}
+
+	/** Used to compare wifis by name their name in alphabetical order */
+	public static class NameComparator implements Comparator<Wifi> {
+
+		@Override
+		public int compare(Wifi w1, Wifi w2) {
+			return w1.getName().compareTo(w2.getName());
+		}
+
+	}
+
+	/** Used to compare wifis by their address in alphabetical order */
+	public static class AddressComparator implements Comparator<Wifi> {
+
+		@Override
+		public int compare(Wifi w1, Wifi w2) {
+			return w1.getAddress().compareTo(w2.getAddress());
+		}
+	}
+
+	/**
+	 * Used to compare wifis by their Status. This comparator will compare the
+	 * wifis in alphabetical order.
+	 * <p>
+	 * The order is: ACTIVE, IN_PROGRESS, IN_FUTURE
+	 */
+	public static class StatusComparator implements Comparator<Wifi> {
+		@Override
+		public int compare(Wifi w1, Wifi w2) {
+			return w1.getStatusString().compareTo(w2.getStatusString());
+		}
+	}
+
+	/**
+	 * Used to compare wifis by their facility type. This comparator will
+	 * compare the wifis in alphabetical order.
+	 * <p>
+	 * The order is: CITY, TRANSIT
+	 */
+	public static class FacilityComparator implements Comparator<Wifi> {
+		@Override
+		public int compare(Wifi w1, Wifi w2) {
+			return w1.getProvider().compareTo(w2.getProvider());
+		}
+	}
+
+	public static class DistanceComparator implements Comparator<Wifi> {
+		@Override
+		public int compare(Wifi w1, Wifi w2) {
+			double d1 = w1.getDistance();
+			double d2 = w2.getDistance();
+
+			if (d1 == d2) {
+				return 0;
+			} else if (d1 != INVALID_DISTANCE && d2 == INVALID_DISTANCE) {
+				// D2 is invalid so d1 must be closer
+				return -1;
+			} else if (d1 == INVALID_DISTANCE && d2 != INVALID_DISTANCE) {
+				// D1 is invalid so d2 must be closer
+				return 1;
+			} else {
+				return (int) (d1 - d2);
+			}
+		}
 	}
 
 }
